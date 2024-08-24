c1b844db76d30c3abe968fbeda29d15bdeb46f81
cody <vishnusanthoshvr@gmail.com>
2024-08-24 21:42:59 +0530

    "Deleted files: foodmenufavicon.png, products page/index.html, script.js, style.css, and multiple images. Updated index.html to change href from '/products page/index.html' to '/productspage/index.html'."

---
+++ productspage/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.Slider' , {
+    autoplay:true,
+    speed:2500,
+    loop:true,
+    parallax:true,
+
+    navigation:{
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+    },
+});
+
+
+
+
